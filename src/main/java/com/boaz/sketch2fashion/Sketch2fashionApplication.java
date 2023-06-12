ea03272d5a26191dc41c0d644881310c022630a6
SangBeom-Hahn <hsb990917@gmail.com>
2023-06-12 17:53:33 +0900

    project start

---
+++ src/main/java/com/boaz/sketch2fashion/Sketch2fashionApplication.java
@@ -0,0 +1,13 @@
+package com.boaz.sketch2fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sketch2fashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sketch2fashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boaz/sketch2fashion/Sketch2fashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boaz.sketch2fashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sketch2fashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
