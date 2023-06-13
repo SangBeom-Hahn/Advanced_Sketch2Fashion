305db4f9f12f67979bf345ca62945eddf5777cba
SangBeom-Hahn <hsb990917@gmail.com>
2023-06-13 19:45:45 +0900

    test: h2 내장 db 테스트 환경 구성

---
+++ src/test/java/com/boaz/sketch2fashion/repository/member/JdbcMemberRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.boaz.sketch2fashion.repository.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Slf4j
+@SpringBootTest
+class JdbcMemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void save() {
+        // given
+
+
+        // when
+
+        // then
+    }
+    @Test
+    void findById() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void findByLoginId() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void update() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void delete() {
+        // given
+
+        // when
+
+        // then
+    }
+}
