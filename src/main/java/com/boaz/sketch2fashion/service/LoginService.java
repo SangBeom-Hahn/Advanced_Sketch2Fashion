8178a05a7ffdba7c112b94f7d76fc9cf46cc4220
SangBeom-Hahn <hsb990917@gmail.com>
2023-06-15 23:49:05 +0900

    feat: 로그인 서비스 로직 작성

---
+++ src/main/java/com/boaz/sketch2fashion/service/LoginService.java
@@ -0,0 +1,18 @@
+package com.boaz.sketch2fashion.service;
+
+import com.boaz.sketch2fashion.domain.Member;
+import com.boaz.sketch2fashion.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(member -> member.getPassword().equals(password))
+                .orElse(null);
+    }
+}
